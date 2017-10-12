0499dda956cdeb116274af8e93ac908a4a140eb3
Max Althoff <althoff@gmail.com>
2017-10-11 19:49:34 -0500

    Greatly reduce complexity of decimal to binary conversion

--- 05/E5_37.java
+++ 05/E5_37.java
@@ -16,47 +16,11 @@ public class E5_37 {
   }
 
   private static String decToBin(int dec) {
-    StringBuilder s = new StringBuilder();
-    int binPlace = highestBinaryPlace(dec);
-    int bitLength = bitLength(binPlace);
-
-    while (s.length() != bitLength) {
-      // tying the loop to binPlace proved troublesome since 1 is a valid
-      // value and binPlace will never be less than 1, so tying it to the
-      // expected number of bits seemed the next best option
-      if (dec >= binPlace) {
-        s.append("1");
-        dec -= binPlace;
-      } else {
-        s.append("0");
-      }
-      binPlace /= 2;
+    StringBuilder bin = new StringBuilder();
+    while (dec != 0) {
+      bin.insert(0, dec % 2);
+      dec /= 2;
     }
-
-    return s.toString();
-  }
-
-  private static int bitLength(int b) {
-    // Obtain the appropriate length of the final string of bits
-    int count = 0;
-    while (true) {
-      count++;
-      if (b == 1) { break; }
-      b /= 2;
-    }
-    return count;
-  }
-
-  private static int highestBinaryPlace(int n) {
-    // Obtain the highest binary place for a given decimal input
-    int binPlace = 1;
-    while (true) {
-      if (binPlace * 2 <= n) {
-        binPlace *= 2;
-      } else {
-        break;
-      }
-    }
-    return binPlace;
+    return bin.toString();
   }
 }
