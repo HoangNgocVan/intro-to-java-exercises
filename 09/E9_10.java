a3e8af8853ecd5e72e71c5cd8621fc85e48b5dd4
Max Althoff <althoff@gmail.com>
2017-10-24 17:32:04 -0500

    Move QuadraticEquation class to its own file

--- 09/E9_10.java
+++ 09/E9_10.java
@@ -37,44 +37,4 @@ public class E9_10 {
       System.out.println("The equation has no roots.");
     }
   }
-
-  private static class QuadraticEquation {
-    private double a;
-    private double b;
-    private double c;
-
-    QuadraticEquation(double a, double b, double c) {
-      this.a = a;
-      this.b = b;
-      this.c = c;
-    }
-
-    // getters
-    double getA() {
-      return a;
-    }
-
-    double getB() {
-      return b;
-    }
-
-    double getC() {
-      return c;
-    }
-
-    // instance methods
-    double getDiscriminant() {
-      return (getB() * getB()) - (4 * getA() * getC());
-    }
-
-    double getRoot1() {
-      if (getDiscriminant() < 0) { return 0; }
-      return (-getB() + Math.sqrt(getDiscriminant())) / (2 * getA());
-    }
-
-    double getRoot2() {
-      if (getDiscriminant() < 0) { return 0; }
-      return (-getB() - Math.sqrt(getDiscriminant())) / (2 * getA());
-    }
-  }
 }
