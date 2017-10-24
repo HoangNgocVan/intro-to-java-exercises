d3f26c09534163c30041cdeadfca37e12b0a737d
Max Althoff <althoff@gmail.com>
2017-10-23 20:11:32 -0500

    Chapter 9, Exercise 9

---
+++ 09/E9_09.java
@@ -0,0 +1,110 @@
+/*
+  In an n-sided regular polygon, all sides have the same length and all angles
+  have the same degree (i.e. the polygon is both equilateral and equiangular).
+  Design a class named RegularPolygon that contains:
+
+  - A private int data field named n that defines the number of sides in the
+    polygon with default value 3.
+  - A private double data field named side that stores the length of the side
+    with default value 1.
+  - A private double data field named x that defines the x-coordinate of the
+    polygon's center with default value 0.
+  - A private double data field named y that defines the y-coordinate of the
+    polygon's center with default value 0.
+  - A no-arg constructor that creates a regular polygon with default values.
+  - A constructor that creates a regular polygon with the specified number of
+    sides and length of side, centered at (0, 0).
+  - A constructor that creates a regular polygon with the specified number of
+    sides, length of side, and x- and y-coordinates.
+  - The accessor and mutator methods for all data fields.
+  - The method getPerimeter() that returns the perimeter of the polygon.
+  - The method getArea() that returns the area of the polygon.
+
+  Write a test program that creates three RegularPolygon objects, created using
+  the no-arg constructor, using RegularPolygon(6, 4), and
+  RegularPolygon(10, 4, 5.6, 7.8). For each object, display its periemter and
+  area.
+*/
+
+public class E9_09 {
+  public static void main(String[] args) {
+    RegularPolygon a = new RegularPolygon();
+    RegularPolygon b = new RegularPolygon(6, 4);
+    RegularPolygon c = new RegularPolygon(10, 4, 5.6, 7.8);
+
+    RegularPolygon[] polys = {a, b, c};
+
+    for (int i = 0; i < polys.length; i++) {
+      System.out.println("Polygon " + i);
+      System.out.println("perimeter: " + polys[i].getPerimeter());
+      System.out.println("area: " + polys[i].getArea());
+      System.out.println();
+    }
+  }
+
+  private static class RegularPolygon {
+    private int n;
+    private double side;
+    private double x;
+    private double y;
+
+    RegularPolygon() {
+      this(3, 1, 0, 0);
+    }
+
+    RegularPolygon(int n, double side) {
+      this(n, side, 0, 0);
+    }
+
+    RegularPolygon(int n, double side, double x, double y) {
+      this.n = n;
+      this.side = side;
+      this.x = x;
+      this.y = y;
+    }
+
+    // getters
+    int getN() {
+      return n;
+    }
+
+    double getSide() {
+      return side;
+    }
+
+    double getX() {
+      return x;
+    }
+
+    double getY() {
+      return y;
+    }
+
+    // setters
+    void setN(int n) {
+      this.n = n;
+    }
+
+    void setSide(double side) {
+      this.side = side;
+    }
+
+    void setX(double x) {
+      this.x = x;
+    }
+
+    void setY(double y) {
+      this.y = y;
+    }
+
+    // instance methods
+    double getPerimeter() {
+      return getN() * getSide();
+    }
+
+    double getArea() {
+      return (getN() * getSide() * getSide()) /
+        (4 * Math.tan(Math.PI / getN()));
+    }
+  }
+}
