b966bbef681e85bc0d39aed7341a216d84956e0c
Max Althoff <althoff@gmail.com>
2017-11-16 16:56:46 -0600

    Use URI to allow for both absolute and relative path entries, and make the stage not resizable

--- 16/E16_10/E16_10.java
+++ 16/E16_10/E16_10.java
@@ -14,7 +14,9 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Button;
+import javafx.geometry.Pos;
 import java.io.File;
+import java.net.URI;
 import java.util.Scanner;
 
 public class E16_10 extends Application {
@@ -35,16 +37,26 @@ public class E16_10 extends Application {
     pane.setBottom(controlPane);
 
     btView.setOnAction(e -> {
-      File file = new File(tfFileName.getText());
-
-      try (
-        Scanner input = new Scanner(file);
-      ) {
-        StringBuilder sb = new StringBuilder();
-        while (input.hasNext()) {
-          sb.append(input.nextLine() + "\n");
+      try {
+        URI uri = new URI(tfFileName.getText());
+        File file;
+        if (uri.isAbsolute()) {
+          file = new File(uri);
+        } else {
+          file = new File(uri.toString());
         }
-        textArea.setText(sb.toString());
+
+        try (
+          Scanner input = new Scanner(file);
+        ) {
+          StringBuilder sb = new StringBuilder();
+          while (input.hasNext()) {
+            sb.append(input.nextLine() + "\n");
+          }
+          textArea.setText(sb.toString());
+        } catch (Exception ex) {
+          textArea.setText(ex.toString());
+        }
       } catch (Exception ex) {
         textArea.setText(ex.toString());
       }
@@ -53,7 +65,10 @@ public class E16_10 extends Application {
     Scene scene = new Scene(pane);
     primaryStage.setTitle("E16_10");
     primaryStage.setScene(scene);
+    primaryStage.setResizable(false);
     primaryStage.show();
+
+    tfFileName.requestFocus();
   }
 
   public static void main(String[] args) {
