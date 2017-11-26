c25ad488931d1cad677bee58b6ecaa6d46226b48
Max Althoff <althoff@gmail.com>
2017-11-25 19:17:48 -0600

    Chapter 17, Exercise 17

---
+++ 17/E17_17/BitOutputStream.java
@@ -0,0 +1,41 @@
+import java.io.FileOutputStream;
+import java.io.File;
+import java.io.IOException;
+
+public class BitOutputStream implements AutoCloseable {
+  private FileOutputStream output;
+  private int bits;
+  private int position;
+
+  public BitOutputStream(File file) throws IOException {
+    output = new FileOutputStream(file);
+  }
+
+  public void writeBit(char bit) throws IOException {
+    bits = bits << 1;
+    if (bit == '1') {
+      bits = bits | 1;
+    }
+
+    position++;
+
+    if (position == 8) {
+      output.write((byte)bits);
+      position = 0;
+    }
+  }
+
+  public void writeBit(String bit) throws IOException {
+    for (int i = 0; i < bit.length(); i++) {
+      char c = bit.charAt(i);
+      writeBit(c);
+    }
+  }
+
+  public void close() throws IOException {
+    bits = bits << (8 - position);
+    output.write((byte)bits);
+    System.out.println(Integer.toBinaryString(bits));
+    output.close();
+  }
+}
