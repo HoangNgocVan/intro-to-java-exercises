dd660b6551a4cdab9697cb5718917cc9467bb9ac
Max Althoff <althoff@gmail.com>
2017-12-03 21:56:34 -0600

    Add problem description

--- 20/E20_11/E20_11.java
+++ 20/E20_11/E20_11.java
@@ -1,4 +1,13 @@
 /*
+  A Java program contains various pairs of grouping symbols, such as:
+
+  - Parentheses: ( and )
+  - Braces: { and }
+  - Brackets: [ and ]
+
+  Note that the grouping symbols cannot overlap. For example, (a{b)} is illegal.
+  Write a program to check whether a Java source-code file has correct pairs of
+  grouping symbols. Pass the source-code file name as a command-line argument.
 */
 
 import java.io.File;
