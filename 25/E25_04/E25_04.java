3e96950a2b3c91fc21e80a92b2d6e4491846fea1
Max Althoff <althoff@gmail.com>
2018-01-20 20:40:05 -0600

    Add user input functionality

--- 25/E25_04/E25_04.java
+++ 25/E25_04/E25_04.java
@@ -4,11 +4,18 @@
   in a BST, and invokes the preorder method to display the elements.
 */
 
+import java.util.Scanner;
+
 public class E25_04 {
   public static void main(String[] args) {
-    Integer[] list = {10, 2, 4, 15, 13, 17, 1};
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter 10 integers: ");
+    Integer[] list = new Integer[10];
+    for (int i = 0; i < list.length; i++) {
+      list[i] = input.nextInt();
+    }
+
     BST<Integer> tree = new BST<>(list);
     tree.preorder();
-    System.out.println();
   }
 }
