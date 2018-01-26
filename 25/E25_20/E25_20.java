04882b98c639f0a9a89965c1823160ab65a01a3b
Max Althoff <althoff@gmail.com>
2018-01-26 14:11:46 -0600

    Add carriage return to maintain line length

--- 25/E25_20/E25_20.java
+++ 25/E25_20/E25_20.java
@@ -46,7 +46,8 @@ public class E25_20 {
     }
 
     for (int i = 0; i < containers.size(); i++) {
-      System.out.print("Container " + (i + 1) + " contains objects with weight ");
+      System.out.print("Container " + (i + 1) +
+        " contains objects with weight ");
       for (int j = 0; j < containers.get(i).size(); j++) {
         System.out.print(containers.get(i).get(j) + " ");
       }
